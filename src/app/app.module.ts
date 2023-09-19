c134db5bdd87c45cb10a5bb2f2b42da04dc898d3
Nuria Castaño <codesalnuria@gmail.com>
2023-09-19 17:05:18 +0200

    add botones new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
+import { BotonesComponent } from './botones/botones.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent
+    ContadorComponent,
+    BotonesComponent
   ],
   imports: [
     BrowserModule
